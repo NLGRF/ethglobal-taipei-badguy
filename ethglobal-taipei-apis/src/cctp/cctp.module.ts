a458bd24e62c20a1e939b4599bd815bb590c92ff
NLGRF <non.devtech@gmail.com>
2025-04-04 20:27:37 +0800

    init and update nest

--- ethglobal-taipei-apis/src/cctp/cctp.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { CctpController } from './cctp.controller';
-import { CctpService } from './cctp.service';
-
-@Module({
-  controllers: [CctpController],
-  providers: [CctpService]
-})
-export class CctpModule {}
