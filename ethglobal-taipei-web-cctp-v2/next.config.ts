89a1a182e5c81d86127c2c3bea4aa1607d06aa28
NLGRF <non.devtech@gmail.com>
2025-04-06 00:58:08 +0800

    v1.0.1

--- ethglobal-taipei-web-cctp-v2/next.config.ts
+++ ethglobal-taipei-web-cctp-v2/next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true, // ✅ ปิด ESLint ใน build
+  },
 };
 
 export default nextConfig;
